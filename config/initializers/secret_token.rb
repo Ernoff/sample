32d40fa086af7e7ebcaa8717706115f525966751
Ernoff <ernest.offiong@gmail.com>
2015-03-13 16:22:23 +0100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackjos::Application.config.secret_key_base = 'ffa2c15db2637a7b1fcd5ce731e31b916e15158ba9e921ce31a453ee1cae2b37930f6786c030f5528371590adf44c13bd4e51d9514689dc281c684f54ce4f049'
